0361aa358eaee044a0ad5747858dc4046f064114
Matthew <matthewcocks@hotmail.com>
2019-12-01 01:16:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ console.log(cards);
 
 console.log(fs.readFileSync('./gfy.txt', 'ascii'));
 
-var port=3001;
+var port=8080;
 var server=app.listen(port,function() {});
 console.log('listening on port '+port+'........ Server ready!');
 console.log('\n\n\thttp://localhost:'+port+'/');
